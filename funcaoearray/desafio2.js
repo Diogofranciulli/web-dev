6b9bb18c0ab83b52a10f48d8e26d034bd7898bc1
Diogo Leles Franciulli <54507748+Diogofranciulli@users.noreply.github.com>
2024-05-15 20:54:33 -0300

    Create desafio2.js

---
+++ funcaoearray/desafio2.js
@@ -0,0 +1,15 @@
+/*  const falar = ['oi, tudo bem?']
+
+
+falar.forEach(
+    function rodarArray(pessoa){
+        console.log(pessoa.toUpperCase())
+    }) */
+
+
+const notas=([
+    {nome:'diogo', media: 5.5},
+    {nome:'diogo', media: 5.5},
+    {nome:'diogo', media: 5.5},
+    {nome:'diogo', media: 5.5},
+])
