99c202aa60bb5e260bb2901d009c3ff85b478ac4
Diogo Leles Franciulli <54507748+Diogofranciulli@users.noreply.github.com>
2024-05-15 20:53:26 -0300

    Create script.js

---
+++ funcaoearray/script.js
@@ -0,0 +1,18 @@
+const pessoas = ['fulano', 'ciclano', 'beltrano']
+
+// add um elemento ao final do array
+pessoas.push("diogo")
+
+//remove o ultimo elemento do array
+pessoas.pop()
+
+// add um elemento no inicio do array
+pessoas.unshift('katia')
+
+// remove o primeiro elemento do array
+pessoas.shift()
+
+// atualizar um elemento do array
+pessoas[1] = "caio"
+
+console.log(pessoas)
